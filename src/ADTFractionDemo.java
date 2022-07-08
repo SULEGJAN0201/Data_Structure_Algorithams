d826b5ba8a64477aa35c2ebd21f1db37a85fb056
sulegjant0201@gmail.com <sulegjant0201@gmail.com>
2022-07-08 12:25:58 +0530

    Basic ADT fraction Structure

---
+++ src/ADTFractionDemo.java
@@ -0,0 +1,54 @@
+public class ADTFractionDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ADTFraction {
+    private int n; //numerator
+    private int d; //denomenator
+
+    public ADTFraction() {
+
+    }
+    //---------------------------------------------------
+    public ADTFraction(int a, int b) {//parameter constructor
+
+    }
+    //---------------------------------------------------
+    public void set(int a, int b) {//set numerator and denomenator
+
+    }
+    //---------------------------------------------------
+    public ADTFraction plus(ADTFraction x) {//add two fractions
+
+    }
+//---------------------------------------------------
+
+    public ADTFraction times(int a) {//multiply fraction by a number
+
+    }
+    //---------------------------------------------------
+
+    public ADTFraction times(ADTFraction x) {//multiply two fractions
+
+    }
+    //---------------------------------------------------
+    public ADTFraction reciprocal() {//reciprocal of a fraction
+
+    }
+    //---------------------------------------------------
+    public float value() {//numerical value of a fraction
+
+    }
+    //---------------------------------------------------
+    public void display() {//display the fraction in the format n/d
+
+    }
+//---------------------------------------------------
+
+    public void refine() {//set the instance to its minimum fraction
+
+        //For instance, if this fraction is 12/30, change it to 2/5.
+    }
+}
