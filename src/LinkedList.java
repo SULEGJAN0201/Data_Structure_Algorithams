3857a3d7cdb5cfc302621eceb08e98f0d6e02649
sulegjant0201@gmail.com <sulegjant0201@gmail.com>
2022-07-12 22:27:37 +0530

    LinkedList start change

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,8 @@
 public class LinkedList {
     public static void main(String[] args) {
         System.out.println("start coding");
+        System.out.println("start coding");
+        System.out.println("start coding");
+
     }
 }
