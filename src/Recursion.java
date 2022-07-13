5a7131f2b99c6bc4f0b46d2813fd4e0d54b0bb85
sulegjant0201@gmail.com <sulegjant0201@gmail.com>
2022-07-13 21:56:20 +0530

    Recursion Basic program

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Recursion {
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the Number :");
+        int number= s.nextInt();
 
+        Recursion R = new Recursion();
+       int value = R.sum(number);
+        System.out.println("Sum of "+number+" is "+value);
+    }
+    public  int sum(int num){
+        if (num==1){
+            return 1;
+        }
+     return   num+ sum(num-1);
     }
 }
